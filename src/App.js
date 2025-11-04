9869970716f540d368378fca57f5c80b31cedeb5
abreoa94-art <abreoa94@gmail.com>
2025-11-04 09:42:55 -0500

    message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import { use, useEffect, useState } from "react";
 
-import Highlights from './components/Highlights';
-import Landing from './components/Landing';
-import Nav from './components/Nav'
 
 function App() {
+const [cart, setCart] = useState ([])
+function addToCart (book){
+  setCart([...cart , book])
+}
+
   return (
-    <div className="App">
-      <Nav />
-     <Landing />
-     <Highlights />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} /> } />
+        <Route path="/books/:id"  render={() =><BookInfo books={books} addToCart={addToCart}/>} />
+        <Route path="/cart"  render={() =><Cart books={books} />} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
